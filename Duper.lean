0e60c013f864aed9e232d7770846c47322d9cc0d
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2025-01-30 23:18:41 -0800

    Add profiling info.

--- Duper.lean
+++ Duper.lean
@@ -2,6 +2,10 @@ import Auto.Tactic
 import Duper.Tactic
 import Auto.EvaluateAuto.TestAuto
 
+def IO.printlnAndFlush {α} [ToString α] (a : α) : IO Unit := do
+  IO.println a
+  (← IO.getStdout).flush
+
 open Lean Auto in
 def duperRaw (lemmas : Array Lemma) (inhs : Array Lemma) : MetaM Expr := do
   let lemmas : Array (Expr × Expr × Array Name × Bool) ← lemmas.mapM
@@ -12,13 +16,15 @@ unsafe def main (args : List String) : IO Unit := do
   let [path] := args | throw $ IO.userError "usage: lake exe duper <path>"
   let line ← IO.FS.readFile path
   let [mod, thm] := line.trim.splitOn " " | throw $ IO.userError "expected: <module> <theorem>"
+  let t0 ← IO.monoMsNow
   Lean.initSearchPath (← Lean.findSysroot)
   Lean.withImportModules #[`Duper, mod.toName] {} 0 fun env => do
+    let t1 ← IO.monoMsNow
+    IO.printlnAndFlush s!"[time] load: {t1 - t0}"
     let coreContext := { fileName := "duper", fileMap := default }
     let coreState := { env }
     let (r, _) ← Lean.Core.CoreM.toIO (EvalAuto.runProverOnConst thm.toName duperRaw) coreContext coreState
+    let t2 ← IO.monoMsNow
+    IO.printlnAndFlush s!"[time] prove: {t2 - t1}"
     let r ← (Lean.toMessageData r).toString
     IO.println r
-
--- #eval main ["benchmarks/Mathlib.Algebra.AddConstMap.EquivAddConstEquiv.refl_apply"]
--- #eval main ["benchmarks/Mathlib.Topology.UniformSpace.SeparationSeparationQuotient.uniformContinuous_lift"]
--- Smt.lean
+++ Smt.lean
@@ -1,14 +1,23 @@
 import Auto.EvaluateAuto.TestAuto
 import Smt.Auto
 
+def IO.printlnAndFlush {α} [ToString α] (a : α) : IO Unit := do
+  IO.println a
+  (← IO.getStdout).flush
+
 unsafe def main (args : List String) : IO Unit := do
   let [path] := args | throw $ IO.userError "usage: lake exe smt <path>"
   let line ← IO.FS.readFile path
   let [mod, thm] := line.trim.splitOn " " | throw $ IO.userError "expected: <module> <theorem>"
+  let t0 ← IO.monoMsNow
   Lean.initSearchPath (← Lean.findSysroot)
   Lean.withImportModules #[`Smt, `Smt.Real, mod.toName] {} 0 fun env => do
+    let t1 ← IO.monoMsNow
+    IO.printlnAndFlush s!"[time] load: {t1 - t0}"
     let coreContext := { fileName := "smt", fileMap := default }
     let coreState := { env }
     let (r, _) ← Lean.Core.CoreM.toIO (EvalAuto.runProverOnConst thm.toName Smt.smtSolverFunc) coreContext coreState
+    let t2 ← IO.monoMsNow
+    IO.printlnAndFlush s!"[time] prove: {t2 - t1}"
     let r ← (Lean.toMessageData r).toString
     IO.println r
