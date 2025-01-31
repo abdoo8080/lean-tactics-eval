cc10a2b9fbd1f05d7be4213280dde01a7ecc5ead
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2025-01-30 21:24:10 -0800

    Cleanup.

--- Smt.lean
+++ Smt.lean
@@ -17,52 +17,9 @@ unsafe def main (args : List String) : IO Unit := do
   let line ← IO.FS.readFile path
   let [mod, thm] := line.trim.splitOn " " | throw $ IO.userError "expected: <module> <theorem>"
   Lean.initSearchPath (← Lean.findSysroot)
-  Lean.withImportModules #[`Smt, mod.toName] ({ {} with  }) 0 fun env => do
+  Lean.withImportModules #[`Smt, mod.toName] {} 0 fun env => do
     let coreContext := { fileName := "smt", fileMap := default }
     let coreState := { env }
     let (r, _) ← Lean.Core.CoreM.toIO (EvalAuto.runProverOnConst thm.toName Smt.smtSolverFunc) coreContext coreState
     let r ← (Lean.toMessageData r).toString
     IO.println r
-
-def test name := do
-  let r ← EvalAuto.runProverOnConst name Smt.smtSolverFunc
-  Lean.logInfo m!"{r}"
-
-#check Int.add_assoc
--- #eval main ["Int.bmod_mul_bmod"]
-
-set_option trace.smt true in
-set_option trace.smt.translate true in
-set_option trace.smt.translate.query true in
--- #eval test ``Int.add_assoc
--- #eval test ``Int.bmod_mul_bmod
-
-set_option auto.native true
-set_option auto.mono.mode "fol"
--- set_option auto.mono.ciInstDefEq.mode "reducible"
--- set_option auto.mono.termLikeDefEq.mode "reducible"
-
-attribute [rebind Auto.Native.solverFunc] Smt.smtSolverFunc
-
--- set_option trace.smt true in
--- set_option trace.smt.translate true in
--- set_option trace.smt.translate.query true in
--- example (a b c : Int) : a + b + c = a + (b + c) := by
---   auto [Int.add_assoc.aux1, Int.add_comm, Int.add_assoc.aux2, Nat.add_assoc, Nat.add_comm, Nat.add_left_comm]
-
--- set_option auto.smt true
--- set_option auto.smt.solver.name "cvc5"
--- set_option auto.smt.trust true
--- set_option trace.auto.smt.printCommands true
-
--- example (a b c : Int) : a + b + c = a + (b + c) := by
---   auto [Int.add_assoc.aux1, Int.add_comm, Int.add_assoc.aux2, Nat.add_assoc, Nat.add_comm, Nat.add_left_comm]
-
-#check Int.succ_neg_succ
-
-def Int.succ (a : Int) := a + 1
-
-set_option trace.smt true in
-open Int in
-example (a : Int) : succ (-succ a) = -a := by
-  smt [Int.succ]
